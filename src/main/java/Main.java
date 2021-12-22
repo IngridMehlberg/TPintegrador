f242710e5ecf610f535d8139379b8b4b3feeafa5
Lucas <Lucas.nmanzolini@outlook.es>
2021-12-21 23:05:30 -0300

    Cambio Brusco 1.2 OJO!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
@@ -5,7 +10,32 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         OperacionesIO op = new OperacionesIO();
-//        --venta vender --vendedor 3 --vehiculo1 124145627457 --output \desktop\ventas.csv
+
+        //        --venta vender --vendedor 3 --vehiculo1 124145627457 --output \desktop\ventas.csv
+
+        ArgumentParser parser = ArgumentParsers.newFor("Proyecto Utn").build()
+                .defaultHelp(true)
+                .description("Concesionaria");
+        parser.addArgument("--venta").type(String.class).required(true);
+        parser.addArgument("--vehiculo").type(String.class).required(true);
+
+
+        Namespace ns = null;
+
+        try {
+            ns = parser.parseArgs(args);
+        } catch (ArgumentParserException e) {
+            parser.handleError(e);
+            System.exit(1);
+        }
+
+        if (ns.getString("venta").equals("vender")){
+            System.out.println("Vendido!");
+        }else{
+            System.out.println("noup.");
+        }
+
+
         LecturaDb lecturaDb = new LecturaDb();
         Venta venta = new Venta();
 
